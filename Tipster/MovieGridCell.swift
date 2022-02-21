84ec1e0948ee8b54066445da0a27d868c63323fe
Noon Elmostafa <nelmostafa@vassar.edu>
2022-02-21 18:37:44 -0500

    Initial Commit

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Noon Elmostafa on 2/14/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
